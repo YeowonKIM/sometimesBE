715043660151b7a5a973126aa96ff089a058d539
1juuun <potenter11@gmail.com>
2023-02-24 22:43:12 +0900

    Feat: 로그인 기능 구현

---
+++ src/main/java/com/sometimes/sometimesbe/service/UserService.java
@@ -0,0 +1,38 @@
+package com.sometimes.sometimesbe.service;
+
+import com.sometimes.sometimesbe.dto.LoginRequestDto;
+import com.sometimes.sometimesbe.entity.User;
+import com.sometimes.sometimesbe.jwt.JwtUtil;
+import com.sometimes.sometimesbe.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Transactional(readOnly = true)
+    public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+
+        String username = loginRequestDto.getUsername();
+        String password = loginRequestDto.getPassword();
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 유저가 없습니다.")
+        );
+
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
+    }
+}

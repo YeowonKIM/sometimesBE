43a2aba1f6253db0213e63c2f440269002a6c503
YeowonKIM <kkyoo152@gmail.com>
2023-02-25 18:22:41 +0900

    Feat: Card Like API 기능 구현 #12
    Card Like API 기능 구현 완료

--- src/main/java/com/sometimes/sometimesbe/repository/CardLikeRepository.java
+++ src/main/java/com/sometimes/sometimesbe/repository/CardLikeRepository.java
@@ -1,4 +1,14 @@
 package com.sometimes.sometimesbe.repository;
 
-public interface CardLikeRepository {
+import com.sometimes.sometimesbe.entity.Card;
+import com.sometimes.sometimesbe.entity.CardLike;
+import com.sometimes.sometimesbe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CardLikeRepository extends JpaRepository<CardLike,Long> {
+    Optional<CardLike> findByCardIdAndUserId(Long cardId, Long userId);
+    Integer countCardLikeByCard_Id(Long cardId);
+
 }
--- src/main/java/com/sometimes/sometimesbe/service/CardService.java
+++ src/main/java/com/sometimes/sometimesbe/service/CardService.java
@@ -2,10 +2,14 @@ package com.sometimes.sometimesbe.service;
 
 import com.sometimes.sometimesbe.dto.CardRequestDto;
 import com.sometimes.sometimesbe.dto.CardResponseDto;
+import com.sometimes.sometimesbe.dto.MessageResponseDto;
 import com.sometimes.sometimesbe.entity.Card;
+import com.sometimes.sometimesbe.entity.CardLike;
 import com.sometimes.sometimesbe.entity.User;
+import com.sometimes.sometimesbe.repository.CardLikeRepository;
 import com.sometimes.sometimesbe.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +23,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CardService {
     private final CardRepository cardRepository;
+    private final CardLikeRepository cardLikeRepository;
 
     @Transactional
     public ResponseEntity<CardResponseDto> createCard(CardRequestDto requestDto, User user) {
@@ -28,13 +33,14 @@ public class CardService {
         return ResponseEntity.ok(cardResponseDto);
     }
 
+    // 카드 전체 조회
     @Transactional
     public ResponseEntity <List<CardResponseDto>> getCards() {
         List<Card> cardList = cardRepository.findAllByOrderByCreatedAtDesc();
         List<CardResponseDto> cardResponseList = new ArrayList<>();
 
         for (Card card : cardList) {
-            cardResponseList.add(CardResponseDto.from(card));
+            cardResponseList.add(CardResponseDto.from(card, cardLikeRepository.countCardLikeByCard_Id(card.getId())));
         }
         return ResponseEntity.ok().body(cardResponseList);
     }
@@ -46,9 +52,28 @@ public class CardService {
         if (card.isEmpty()) {
             throw new IllegalArgumentException("해당 카드가 없습니다.");
         }
-        return ResponseEntity.ok()
-                .body(CardResponseDto.from(card.get()));
+        return ResponseEntity.ok().body(CardResponseDto.from(card.get(), cardLikeRepository.countCardLikeByCard_Id(card.get().getId())));
 
     }
+
+    // 카드 좋아요
+    @Transactional
+    public ResponseEntity<MessageResponseDto> createLike(Long id, User user) {
+        Card card = cardRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 카드가 없습니다.")
+        );
+
+        Optional<CardLike> cardLike = cardLikeRepository.findByCardIdAndUserId(id, user.getId());
+        if (cardLike.isEmpty()) {  // 좋아요
+            cardLikeRepository.saveAndFlush(CardLike.of(card, user));
+            return ResponseEntity.ok().body(MessageResponseDto.of("좋아요 추가", HttpStatus.OK));
+        } else {  // 좋아요 취소
+            cardLikeRepository.delete(cardLike.get());
+            return ResponseEntity.ok().body(MessageResponseDto.of("좋아요 취소",HttpStatus.OK));
+        }
+
+
+    }
+
 
 }
